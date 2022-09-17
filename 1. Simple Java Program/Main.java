1b396971c157c85bcf47e9e0247d781c6f8098e4
SaurabhPagrut <saurabhpagrut2016@gmail.com>
2022-09-17 23:04:57 +0530

    added first java program on github

---
+++ 1. Simple Java Program/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Saurabh";
+        int age = 22;
+        System.out.println("Hello Everyone My Name is : " + name);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("See you later!");
+    }
+}
